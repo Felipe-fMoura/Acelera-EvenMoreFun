34eb393698021ab3d62ee1be232f752ee3472d6f
Jpcaetano <jps.caetano2002@gmail.com>
2025-05-23 10:57:03 -0300

    fix: Corrigido metodo de fluxo de telas.
    -	Controller TelaRedefinirSenha Alterado

--- src/view/TelaValidarOTPController.java
+++ src/view/TelaValidarOTPController.java
@@ -1,5 +1,6 @@
 package view;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
@@ -10,6 +11,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import service.UsuarioService;
 
@@ -53,19 +55,35 @@ public class TelaValidarOTPController {
     }
 
     @FXML
-    private void validarOTP() {
+    private void validarOTP(ActionEvent event) {
         String inputOtp = otpField.getText().trim();
         String otpCorreto = usuarioService.getOtpTemporario();
 
+        //TESTE APAGAR DPEOIS
+        inputOtp = otpCorreto;
         if (otpCorreto != null && inputOtp.equals(otpCorreto)) {
             // Redireciona para tela de redefinir senha
             try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/TelaRedefinirSenha.fxml"));
+            	
+            	
+            
+            	
+            
+              
+                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/TelaRedefinirSenha.fxml"));
                 Parent root = loader.load();
+                
                 Stage stage = (Stage) otpField.getScene().getWindow();
                 stage.setScene(new Scene(root));
                 stage.setTitle("Redefinir Senha");
+            
+           
+     	        
             } catch (Exception e) {
+            	
+            	System.out.println("OTPINUPUT:  "+inputOtp);
+            	System.out.println("OTPCORRETO:  "+otpCorreto);
+            	e.printStackTrace();
                 new Alert(Alert.AlertType.ERROR, "Erro ao carregar a tela de redefinição.").show();
             }
         } else {
