bb4dd9b21969d59cd52245ae98a0bbbd58670c5b
Felipe-fMoura <fmoura.dev@gmail.com>
2025-06-20 13:10:30 -0300

    feat: Atualizações visuais True
    - Alterações na tela Perfil, Menu

--- src/view/TelaMenuController.java
+++ src/view/TelaMenuController.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -53,6 +54,9 @@ public class TelaMenuController {
     
     @FXML
     private VBox testeVbox;
+    
+    @FXML
+    private ScrollPane scrollPane;
     
     @FXML
     private Text txtRandom;
